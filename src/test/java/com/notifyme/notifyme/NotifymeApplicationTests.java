c921ae71127ddf8e55072d5bf1ea3bcb7cbc77e8
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2025-12-12 14:21:22 -0400

    first commit

---
+++ src/test/java/com/notifyme/notifyme/NotifymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifyme.notifyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
